d8c6522609b6066ba6f90e7b63a245707881621a
Karsten <kcole1371@bths.edu>
2022-06-16 20:40:13 -0400

    removed fps count

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -38,7 +38,6 @@ public class GamePanel extends JPanel implements Runnable{
         long lastTime = System.nanoTime();
         long currentTime;
         long timer = 0;
-        int drawCount = 0;
 
 
         while(gameThread != null){
@@ -53,15 +52,7 @@ public class GamePanel extends JPanel implements Runnable{
                 update();
                 repaint();
                 delta--;
-                drawCount++;
             }
-
-            if(timer >= 1000000000){
-                System.out.println("FPS: "+ drawCount);
-                drawCount = 0;
-                timer = 0;
-            }
-
         }
 
     }
